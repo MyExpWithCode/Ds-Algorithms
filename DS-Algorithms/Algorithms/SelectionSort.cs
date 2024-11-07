71efaa657ffba52a0cacde1ad4e89925ba1e6a57
Nithesh Reddy <Nithesh_Reddy@hotmail.com>
2024-11-08 00:26:16 +0530

    restrcture the project

---
+++ DS-Algorithms/Algorithms/SelectionSort.cs
@@ -0,0 +1,28 @@
+﻿namespace DS_Algorithms.Algorithms
+{
+    // Selection sort searchs for the small element and orders the ascending order
+    // Time Complexicity : O()
+    // Memory Complexicity : O()
+    public static class SelectionSort
+    {
+        public static int[] RunSelectionSort(int[] arr)
+        {
+            // [3,7,2,9,4,1]
+            var arrayLength = arr.Length; // T:1, M:4B
+
+            for (int i = 0; i < arrayLength; i++) // T:n
+            {
+                for (int j = i+1; j < arrayLength; j++) // T: n*(n-1)/2
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int position = arr[i];
+                        arr[i]= arr[j];
+                        arr[j]= position;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
