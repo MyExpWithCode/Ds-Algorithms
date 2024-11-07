6c7a885ad68012364c7a4dc8610757a7f075745a
Nithesh Reddy <Nithesh_Reddy@hotmail.com>
2024-11-08 00:23:16 +0530

    Selection Sort

--- DS-Algorithms/Program.cs
+++ DS-Algorithms/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace DS_Algorithms
+﻿using LeetCode;
+
+namespace DS_Algorithms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            // Selection Sort : 07/11/2024
+            //var arr = SelectionSort.RunSelectionSort(new int[] { 3, 9, 5, 10, 1 });
+            //Console.WriteLine(arr);
         }
     }
 }
